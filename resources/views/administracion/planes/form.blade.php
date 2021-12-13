235f1bf1ed39df9f3243b1c9329f487a924b7e72
Yonathan Bohorquez <ycbohorquez@ucundinamarca.edu.co>
2021-12-13 16:57:11 -0500

    Formulario de planes

---
+++ resources/views/administracion/planes/form.blade.php
@@ -0,0 +1,14 @@
+<div class="form-body">
+    <div class="form-group">
+        <h5 class="mt-2">Nombre</h5>
+        <input type="text" id="TTR_Nombre_Tipo_Reserva" class="form-control" placeholder="Nombre" name="TTR_Nombre_Tipo_Reserva">
+    </div>
+    <div class="form-group">
+        <h5 class="mt-2">Descripción</h5>
+        <input type="text" id="TTR_Descripcion_Tipo_Reserva" class="form-control" placeholder="Descripción" name="TTR_Descripcion_Tipo_Reserva">
+    </div>
+    <div class="form-group">
+        <h5 class="mt-2">Valor</h5>
+        <input type="number" min="0" id="TTR_Costo_Tipo_Reserva" class="form-control" placeholder="Valor" name="TTR_Costo_Tipo_Reserva">
+    </div>
+</div>
